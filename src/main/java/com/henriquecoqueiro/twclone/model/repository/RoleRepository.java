a0cd37e8f97b4889d6291e4dfd72d603aa870ba4
HenriqueCoqueiro <henriquecoqueiromelo@gmail.com>
2025-05-30 20:06:11 -0300

    Repository notation fixed

--- src/main/java/com/henriquecoqueiro/twclone/model/repository/RoleRepository.java
+++ src/main/java/com/henriquecoqueiro/twclone/model/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package com.henriquecoqueiro.twclone.model.repository;
 
 import com.henriquecoqueiro.twclone.model.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
--- src/main/java/com/henriquecoqueiro/twclone/model/repository/TweetRepository.java
+++ src/main/java/com/henriquecoqueiro/twclone/model/repository/TweetRepository.java
@@ -3,6 +3,8 @@ package com.henriquecoqueiro.twclone.model.repository;
 import com.henriquecoqueiro.twclone.model.entities.Role;
 import com.henriquecoqueiro.twclone.model.entities.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
 }
